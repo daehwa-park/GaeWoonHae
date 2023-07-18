21a0f5cee5b6b44efafd43de4d85df78b7a16f48
dntmdqls0912 <dntmdqls0912@gmail.com>
2023-07-18 12:58:44 +0900

    user controller and service

--- Server/src/main/java/com/threeracha/gaewoonhae/db/domain/Room.java
+++ Server/src/main/java/com/threeracha/gaewoonhae/db/domain/Room.java
@@ -1,6 +1,11 @@
 package com.threeracha.gaewoonhae.db.domain;
 
-
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED) //기본 생성자 만들어줌
 @DynamicUpdate //update 할때 실제 값이 변경됨 컬럼으로만 update 쿼리를 만듬
@@ -10,7 +15,7 @@ package com.threeracha.gaewoonhae.db.domain;
 @Table(name = "room") //테이블 관련 설정 어노테이션
 public class Room {
 
-    @Session_id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "session_id")
     private String session_id;
@@ -23,7 +28,5 @@ public class Room {
 
     @Column(name = "is_public_room")
     private int is_public_room;
-
-
 
 }
--- Server/src/main/java/com/threeracha/gaewoonhae/db/domain/User.java
+++ Server/src/main/java/com/threeracha/gaewoonhae/db/domain/User.java
@@ -1,8 +1,14 @@
 package com.threeracha.gaewoonhae.db.domain;
 
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED) //기본 생성자 만들어줌
+@NoArgsConstructor //기본 생성자 만들어줌
 @DynamicUpdate //update 할때 실제 값이 변경됨 컬럼으로만 update 쿼리를 만듬
 @Entity //JPA Entity 임을 명시
 @Getter //Lombok 어노테이션으로 getter
@@ -10,10 +16,10 @@ package com.threeracha.gaewoonhae.db.domain;
 @Table(name = "user") //테이블 관련 설정 어노테이션
 public class User {
 
-    @User_id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private int user_id;
+    private int userId;
 
     @Column(name = "nickname", nullable = false)
     private String nickname;
@@ -22,8 +28,6 @@ public class User {
     private String email;
 
     @Column(name = "refresh_token", nullable = false)
-    private String refresh_token;
-
-
+    private String refreshToken;
 
 }
