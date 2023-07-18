1d22b4d5998f6f999e1864c5f63ad3225966808a
subin <qotnqls77@naver.com>
2023-07-18 13:07:38 +0900

    어노테이션 수정

--- Server/src/main/java/com/threeracha/gaewoonhae/db/domain/User.java
+++ Server/src/main/java/com/threeracha/gaewoonhae/db/domain/User.java
@@ -15,7 +15,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private int user_id;
+    private int userId;
 
     @Column(name = "nickname", nullable = false)
     private String nickname;
@@ -24,6 +24,10 @@ public class User {
     private String email;
 
     @Column(name = "refresh_token", nullable = false)
+<<<<<<< HEAD
     private String refresh_token;
+=======
+    private String refreshToken;
+>>>>>>> cf9161407c640a3ec549fa6438e730129b665af5
 
 }
