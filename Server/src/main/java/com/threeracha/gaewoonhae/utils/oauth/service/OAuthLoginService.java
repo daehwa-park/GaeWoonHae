55fc46668a2ccac3d07c065e792198f79871387f
dntmdqls0912 <dntmdqls0912@gmail.com>
2023-07-25 14:06:34 +0900

    modify refreshtoken save

--- Server/src/main/java/com/threeracha/gaewoonhae/utils/oauth/service/OAuthLoginService.java
+++ Server/src/main/java/com/threeracha/gaewoonhae/utils/oauth/service/OAuthLoginService.java
@@ -18,10 +18,20 @@ public class OAuthLoginService {
     private final RequestOAuthInfoService requestOAuthInfoService;
 
     public AuthTokens login(OAuthLoginParams params) {
+        // API 서버로부터 유저 정보를 받아옴
         OAuthInfoResponse oAuthInfoResponse = requestOAuthInfoService.request(params);
+        
+        // 유저 정보를 바탕으로 신규 유저는 회원가입을, 기존 유저는 로그인을 실행
         User user = findOrCreateUser(oAuthInfoResponse);
-        AuthTokens tokens = authTokensGenerator.generate(user.getUserId());
-        return LoginResponse.builder().tokens(tokens).nickname(user.getNickname())
+        
+        // 로그인 한 유저 ID를 바탕으로 accessToken과 refreshToken 생성
+        AuthTokens token = authTokensGenerator.generate(user.getUserId());
+
+        // user 테이블에 refreshToken 저장
+        user.setRefreshToken(token.getRefreshToken());
+        userRepository.flush();
+
+        return token;
     }
 
     private User findOrCreateUser(OAuthInfoResponse oAuthInfoResponse) {
