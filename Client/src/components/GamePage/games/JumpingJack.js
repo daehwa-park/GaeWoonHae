885b6abbe6c60db12d1e6154bfece16bdcf481e0
dntmdqls0912 <dntmdqls0912@gmail.com>
2023-08-03 17:52:52 +0900

    ADD: 게임 컴포넌트 분리

---
+++ Client/src/components/GamePage/games/JumpingJack.js
@@ -0,0 +1,43 @@
+import React, { Component, useEffect, useState } from 'react';
+import { useSelector } from "react-redux/es/hooks/useSelector";
+
+const JumpingJack = (props) => {
+
+    const count = props.count;
+    const setCount = props.setCount;
+    const limitTime = props.limitTime;
+    const started = props.started;
+    const setStarted = props.setStarted;
+    const bakHP = 5;
+    
+    const jumpingJackCheck = () => {
+        // 티처블머신으로 자세에 따라 카운트 증가
+    }
+
+
+    useEffect(() => {
+        if (started) {
+            setTimeout(()=> {
+                startTimer();
+            }, 5000)
+        }
+    },[started]);
+
+    const startTimer = () => {
+
+    }
+
+    // 박 흔들기
+    const jitterBak = () => {
+
+    }
+
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default JumpingJack;
--- Client/src/room/GamePage/Game.js
+++ Client/src/room/GamePage/Game.js
@@ -6,33 +6,58 @@ import Webcam from "react-webcam";
 import { loadHaarFaceModels, detectHaarFace } from "../../features/openvidu_opencv/opencv/haarFaceDetection";  // 얼굴인식 컴포넌트
 import cv from "@techstark/opencv-js";
 import {Link} from 'react-router-dom'
-// 게임페이지2 - 픽토그램
+import { useSelector } from "react-redux/es/hooks/useSelector"
+import JumpingJack from '../../components/GamePage/games/JumpingJack';
 
-const GamePage = (props) => {
+// 게임페이지
+
+const GamePage = () => {
 
     const APPLICATION_SERVER_URL = process.env.NODE_ENV === 'production' ? 'https://i9b303.p.ssafy.io/' : 'https://demos.openvidu.io/';
+    const hostName = useSelector((state) => state.roomInfo.hostName);
+    const myName = useSelector((state) => state.auth.user.nickname);
+    const sessionId = useSelector((state) => state.roomInfo.sessionId);
+    const gameType = useSelector((state) => state.roomInfo.gameType);
+    const limitTime = 0;
 
-    const [mySessionId, setMySessionId] = useState(props.sessionId);
-    const [myUserName, setMyUserName] = useState(props.nickname);
-    const [session, setSession] = useState(undefined);
     const [mainStreamManager, setMainStreamManager] = useState(undefined);
     const [publisher, setPublisher] = useState(undefined);
     const [subscriber, setSubscriber] = useState([]);
+    const [count, setCount] = useState(0);
+    const [started, setStarted] = useState(false);
+    const [finished, setFinished] = useState(false);
 
     const webcamRef = useRef();
     const imgRef = useRef();
     const faceImgRef = useRef();
     const emoji = useRef();
 
+    useEffect(() => {
 
+    },[])
 
+    // OpenVidu, Stomp 커넥션 하세요
+    
+    // 게임 종료 신호
+    useEffect(() => {
+        if (finished) {
+            // 결과 전송 및 게임 결산
+        }
+    },[finished])
 
-
-
+    // 게임 카운트 갱신
+    useEffect(()=> {
+        // 소켓으로 자신의 카운트가 바뀐것을 다른사람에게 알림
+    },[count])
 
     return (
         <div>
- 
+            {/* 네비 */}
+            {/* 내 운동 화면 */}
+            {/* 게임 종류별 컴포넌트 */}
+            {/* ex) <JumpingJack props={} /> */}
+            {/* for i in 4  */}
+            {/*     다른사람 화면 */}
         </div>
     )
 }
