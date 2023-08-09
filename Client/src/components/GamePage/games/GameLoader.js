1724991b93a90b14d0fee0fa856b87b049bfb410
dntmdqls0912 <dntmdqls0912@gmail.com>
2023-08-09 16:05:43 +0900

    checkpoint

--- Client/src/components/GamePage/games/GameLoader.js
+++ Client/src/components/GamePage/games/GameLoader.js
@@ -10,29 +10,62 @@ const GameLoader = ({props}) => {
     const finished = props.finished;
     const gameType = props.gameType;
     const setGameLoad = props.setGameLoad;
+    const limitTime = props.limitTime;
 
     // states
     const [model, setModel] = useState();
     const [webcam, setWebcam] = useState();
+    const [poseList, setPoseList] = useState([]);
+    const [targetList, setTargetList] = useState([]);
 
-    const poseList = useRef([0,1,2,3,4]);
+    let step = 0;
+    let isPosing = false;
 
-    let URL;
-    let loopId;
-    let picTimerId;
-    let squatTimerId;
-    // let poseList = [0,1,2,3,4];
-    let poseIndex = 0;
-
+    // catch mosquito
     let ready = false;
     let set = false;
-    let go = false;
+
+
+    // const poseList = useRef([0,1,2,3,4]);
+
+    let URL;
+    let loopWebcamId;
+    let loopPredId;
+    let squatTimerId;
+    let poseIndex = 0;
+
     
     let rightReady = false;
     let leftReady = false;
 
     let ballPos = 0;
     let startTime;
+
+
+
+    const randomNumbers = () => {
+        let arr = [];
+        let num;
+
+        if (gameType === 1) {
+            while (arr.length <= limitTime/2) {
+                num = Math.floor(Math.random()*4);
+                if (!arr || arr[-1] !== num) {
+                    arr.push(num);
+                }
+            }
+
+        } else if (gameType === 2) {
+            while (arr.length <= limitTime/5) {
+                num = Math.floor(Math.random()*10);
+                if (!arr || arr[-1] !== num) {
+                    arr.push(num);
+                }
+            }
+        }
+        return arr;
+    }
+
 
     /*
     * Teachable Machine Settings
@@ -43,9 +76,13 @@ const GameLoader = ({props}) => {
         switch(gameType) {
             case 1: 
                 URL = 'https://teachablemachine.withgoogle.com/models/ZWOxIpSRc/';
+                setPoseList([0, 1, 2, 3]);
+                setTargetList(randomNumbers());
                 break;
             case 2: 
                 URL = 'https://teachablemachine.withgoogle.com/models/99dOWJKg2/';
+                setPoseList([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+                setTargetList(randomNumbers());
                 break;
             case 3: 
                 URL = 'https://teachablemachine.withgoogle.com/models/PP6A_2GsN/';
@@ -76,83 +113,88 @@ const GameLoader = ({props}) => {
         setModel(await window.tmPose.load(modelURL, metadataURL));
     }
     
+    
     const predictJumpingJack = async () => {
         if (model && webcam) {
             const {pose, posenetOutput} = await model.estimatePose(webcam.canvas);
-    
             const prediction = await model.predict(posenetOutput);
 
-            if (!ready && prediction[0].probability > 0.85) {
-                ready = true;
-                console.log("ready -> true");
-            }
-            else if (ready && !set && prediction[1].probability > 0.85) {
+            if (!set &&  prediction[0].probability > 0.85) {
                 set = true;
                 console.log("set -> true");
             }
-            else if (ready && set  && !go && prediction[0].probability > 0.85) {
+            
+            else if (set && !ready2 && prediction[1] > 0.85) {
+                ready2 = true;
+                console.log("ready2 -> true");
+            }
+
+            
+
+
+            else if (set && ready2 && !go && prediction[2].probability > 0.85) {
                 go = true;
                 console.log("go -> true");
             }
             else if (go) {
                 setCount(prev => prev + 1);
-                ready = set = go = false;
-                console.log("complete", ready, set, go);
+                ready2 = set = go = false;
+                console.log("complete", set, ready2, go);
             }
         }
     }
 
-    const predictPictogram = async () => {
-        if (model && webcam) {
-            const {pose, posenetOutput} = await model.estimatePose(webcam.canvas);
+    // const predictPictogram = async () => {
+    //     if (model && webcam) {
+    //         const {pose, posenetOutput} = await model.estimatePose(webcam.canvas);
     
-            const prediction = await model.predict(posenetOutput);
+    //         const prediction = await model.predict(posenetOutput);
 
-            if (prediction[poseList.current[poseIndex]].probability > 0.85) {
-                setCount(prev => prev + 1);
-                poseIndex++;
+    //         if (prediction[poseList.current[poseIndex]].probability > 0.85) {
+    //             setCount(prev => prev + 1);
+    //             poseIndex++;
                 
-                if(poseIndex == poseList.length){
-                    poseIndex = 0;
-                }
-                console.log(poseIndex);
-            }
-            else {
-                poseIndex = 0;
-                console.log(poseIndex);
-            }
-        }
-    }
+    //             if(poseIndex == poseList.length){
+    //                 poseIndex = 0;
+    //             }
+    //             console.log(poseIndex);
+    //         }
+    //         else {
+    //             poseIndex = 0;
+    //             console.log(poseIndex);
+    //         }
+    //     }
+    // }
 
-    const predictSquat = async () => {
-        if (model && webcam) {
-            const {pose, posenetOutput} = await model.estimatePose(webcam.canvas);
+    // const predictSquat = async () => {
+    //     if (model && webcam) {
+    //         const {pose, posenetOutput} = await model.estimatePose(webcam.canvas);
     
-            const prediction = await model.predict(posenetOutput);
+    //         const prediction = await model.predict(posenetOutput);
 
-            console.log(ballPos, "squat!!");
+    //         console.log(ballPos, "squat!!");
             
 
-            if (ballPos == 0 && !rightReady && prediction[1].probability > 0.85) {
-                rightReady = true;
-                console.log("Rready -> true");
-            }
-            else if (ballPos == 0 && rightReady && prediction[0].probability > 0.85) {
-                setCount(prev => prev + 1);
-                rightReady = false;
-                console.log("complete 0");
-            }
-            else if (ballPos == 1 && !leftReady && prediction[3].probability > 0.85) {
-                leftReady = true;
-                console.log("Lready -> true");
-            }
-            else if (ballPos == 1 && leftReady && prediction[2].probability > 0.85) {
-                setCount(prev => prev + 1);
-                leftReady = false;
-                console.log("complete 1");
-            }
-        }
-    }
+    //         if (ballPos == 0 && !rightReady && prediction[1].probability > 0.85) {
+    //             rightReady = true;
+    //             console.log("Rready -> true");
+    //         }
+    //         else if (ballPos == 0 && rightReady && prediction[0].probability > 0.85) {
+    //             setCount(prev => prev + 1);
+    //             rightReady = false;
+    //             console.log("complete 0");
+    //         }
+    //         else if (ballPos == 1 && !leftReady && prediction[3].probability > 0.85) {
+    //             leftReady = true;
+    //             console.log("Lready -> true");
+    //         }
+    //         else if (ballPos == 1 && leftReady && prediction[2].probability > 0.85) {
+    //             setCount(prev => prev + 1);
+    //             leftReady = false;
+    //             console.log("complete 1");
+    //         }
+    //     }
+    // }
 
     useEffect(() => {
         
@@ -162,72 +204,81 @@ const GameLoader = ({props}) => {
             console.log("GAME LOADED!!!!!!!!!!!!!");
         }
 
-        const shuffle = (array) => {
-            for (let i = array.length - 1; i > 0; i--) {
-                  // 무작위로 index 값 생성 (0 이상 i 미만)
-              let j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-            }
-        }
-
-        shuffle(poseList.current);
-        console.log(poseList.current);
         init();
     },[])
 
 
     useEffect(() => {
 
-        const loop = async () => {
+        const loopWebcam = async () => {
             webcam.update();
 
-            loopId = requestAnimationFrame(loop);
+            loopWebcamId = requestAnimationFrame(loopWebcam);
         };
 
-        const predictLoop = async (timestamp) => {
-            
+        const loopPredict = async () => {
             switch(gameType) {
                 case 1:
-                    await predictJumpingJack();
-                    requestAnimationFrame(predictLoop);
+                    loopPredId = setTimeout(() => {
+
+
+
+
+                    })
                     break;
                 case 2:
-                    picTimerId = setInterval(() => {
-                        console.log("interval");
-                        console.log("nextpose", poseList.current[poseIndex]);
-                        setTimeout(() => {
-                            console.log("ready");
-                            setTimeout(()=> {
-                                predictPictogram();
-                            },3000);
-                        }, 2000);
-                    }, 5000);
-                    break;
-                case 3:
-                    if(!startTime) {
-                        startTime = timestamp;
-                    }
 
-                    let currentTime = timestamp - startTime;
-                    currentTime = Math.floor(currentTime);
-                    console.log(currentTime);
-                    if(currentTime > 5000) {
-                        startTime = timestamp;
-                        ballPos = Math.floor(Math.random() * 2);
-                        console.log(timestamp - startTime, ballPos);
-                    }
-                    
-                    await predictSquat();
-                    requestAnimationFrame(predictLoop);
-                    break;
+                case 3:
+
                 default:
-                    break;
+
             }
         }
 
+        // const predictLoop = async (timestamp) => {
+            
+        //     switch(gameType) {
+        //         case 1:
+        //             await predictJumpingJack();
+        //             requestAnimationFrame(predictLoop);
+        //             break;
+        //         case 2:
+        //             picTimerId = setInterval(() => {
+        //                 console.log("interval");
+        //                 console.log("nextpose", poseList.current[poseIndex]);
+        //                 setTimeout(() => {
+        //                     console.log("ready");
+        //                     setTimeout(()=> {
+        //                         predictPictogram();
+        //                     },3000);
+        //                 }, 2000);
+        //             }, 5000);
+        //             break;
+        //         case 3:
+        //             if(!startTime) {
+        //                 startTime = timestamp;
+        //             }
+
+        //             let currentTime = timestamp - startTime;
+        //             currentTime = Math.floor(currentTime);
+        //             console.log(currentTime);
+        //             if(currentTime > 5000) {
+        //                 startTime = timestamp;
+        //                 ballPos = Math.floor(Math.random() * 2);
+        //                 console.log(timestamp - startTime, ballPos);
+        //             }
+                    
+        //             await predictSquat();
+        //             requestAnimationFrame(predictLoop);
+        //             break;
+        //         default:
+        //             break;
+        //     }
+        // }
+
         if (started)  {
-            loop();
-            predictLoop();
+            loopWebcam();
+            loopPredict();
             console.log("TIMER START!!!!!!!!!!!!!!")
         }
 
@@ -235,8 +286,8 @@ const GameLoader = ({props}) => {
 
     useEffect(() => {
 
-        cancelAnimationFrame(loopId);
-        clearInterval(picTimerId);
+        cancelAnimationFrame(loopWebcamId);
+        clearInterval(loopPredId);
         // clearInterval(squatTimerId);
 
     }, [finished])
