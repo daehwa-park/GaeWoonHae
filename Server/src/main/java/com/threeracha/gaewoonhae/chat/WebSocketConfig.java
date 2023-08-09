35bd7b31445cd917eee12ccd139148ebae613326
박대화 <zlzl_321@naver.com>
2023-08-09 16:24:20 +0900

    FIX: websocket connection error fix

--- Server/src/main/java/com/threeracha/gaewoonhae/chat/WebSocketConfig.java
+++ Server/src/main/java/com/threeracha/gaewoonhae/chat/WebSocketConfig.java
@@ -22,7 +22,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gwh-websocket").withSockJS(); // 커넥션을 맺는 경로 설정. 만약 WebSocket을 사용할 수 없는 브라우저라면 다른 방식을 사용하도록 설정
+        registry.addEndpoint("/gwh-websocket")
+                .setAllowedOriginPatterns("*")
+                .withSockJS(); // 커넥션을 맺는 경로 설정. 만약 WebSocket을 사용할 수 없는 브라우저라면 다른 방식을 사용하도록 설정
     }
 
     public void configureClientInboundChannel(ChannelRegistration registration) {
