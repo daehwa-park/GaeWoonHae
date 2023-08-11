e5d204d394d28ad625ec383348a9f29fee5a57cd
dntmdqls0912 <dntmdqls0912@gmail.com>
2023-08-11 12:33:01 +0900

    FIX: 포즈인식 모드로 변경

--- Client/src/components/GamePage/games/ui/JumpingJack.js
+++ Client/src/components/GamePage/games/ui/JumpingJack.js
@@ -1,9 +1,70 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component, useEffect, useState, useRef } from 'react';
+import mosquito from '../../../../assets/game/jumpingjack/mosquito.png'
+import { Col, Row } from 'react-bootstrap';
+import { isVisible } from '@testing-library/user-event/dist/utils';
+const JumpingJack = ({props}) => {
 
-const JumpingJack = () => {
+    const setAssetLoad = props.setAssetLoad;
+    const curPoseState = props.curPoseState;
+    const success = props.success;
+    const fail = props.fail;
+
+    const normalImages = [
+        useRef(),
+        useRef(),
+        useRef(),
+        useRef()
+    ];
+
+    const successImages = [
+        useRef(),
+        useRef(),
+        useRef(),
+        useRef()
+    ];
+
+    const curPose = useRef();
+    const failImage = useRef();
+
+
+    useEffect(() => {
+
+    },[]);
+
+    useEffect(() => {
+        if (curPoseState) {
+            curPose.current = curPoseState;
+
+        }
+    },[curPoseState]);
+
+
+
     return (
         <div>
-            
+            <Col>
+                <Row>
+                    <div>
+                        <img src={mosquito} ref={normalImages[0]} style={isVisible=false} />
+                        <img src={mosquito} ref={successImages[0]} style={isVisible=false} />
+                    </div>
+                    <div>
+                        <img src={mosquito} ref={normalImages[1]} style={isVisible=false} />
+                        <img src={mosquito} ref={successImages[1]} style={isVisible=false} />
+                    </div>
+                </Row>
+                <Row>
+                    <div>
+                        <img src={mosquito} ref={normalImages[2]} style={isVisible=false} />
+                        <img src={mosquito} ref={successImages[2]} style={isVisible=false} />
+                    </div>
+                    <div>
+                        <img src={mosquito} ref={normalImages[3]} style={isVisible=false} />
+                        <img src={mosquito} ref={successImages[3]} style={isVisible=false} />
+                    </div>
+                </Row>
+            </Col>
+            <img src={mosquito} ref={failImage} style={isVisible=false} />
         </div>
     );
 };
