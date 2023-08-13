908c5264896e35b2dad744c1ae44893946550840
subin <qotnqls77@naver.com>
2023-08-14 00:02:47 +0900

    fix:main 변경

--- Client/src/components/mypage/myprofile.js
+++ Client/src/components/mypage/myprofile.js
@@ -330,43 +330,39 @@ const Myprofilepage = () => {
                     // selectRange={true} 
                     formatDay={(locale, date) => moment(date).format("DD")} 
                     /> 
-                    {/* <Calendar
-                      onChange={onCalendarChange} // useState로 포커스 변경 시 현재 날짜 받아오기
-                      formatDay={(locale, date) => moment(date).format("DD")} // 날'일' 제외하고 숫자만 보이도록 설정
-                      value={value}
-                      minDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
-                      maxDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
-                      navigationLabel={null}
-                      showNeighboringMonth={false} //  이전, 이후 달의 날짜는 보이지 않도록 설정
-                      className="mx-auto w-full text-sm border-b"
-                      tileContent={({ date, view }) => { // 날짜 타일에 컨텐츠 추가하기 (html 태그)
-                        // 추가할 html 태그를 변수 초기화
-                        let html = [];
-                        // 현재 날짜가 post 작성한 날짜 배열(mark)에 있다면, dot div 추가
-                        if (mark.find((x) => x === moment(date).format("YYYY-MM-DD"))) {
-                          html.push(<div className="dot"></div>);
-                        }
-                        // 다른 조건을 주어서 html.push 에 추가적인 html 태그를 적용할 수 있음.
-                        return (
-                          <>
-                            <div className="flex justify-center items-center absoluteDiv">
-                              {html}
-                            </div>
-                          </>
-                        );
-                      }}
-                    /> */}
+                 
                 </div>
 
                 <div className='image3'>
-                    <div className='kcal'>오늘 소모 칼로리 : {todayKcal} Kcal</div>
+                    {/* <div className='kcal'>오늘 소모 칼로리 : {todayKcal} Kcal</div>
                     <div className='kcal'>총 소모 칼로리 : {totalKcal} Kcal</div>
                     <p></p>
 
                     <div className='kcal'>선택 일자 : 
                         {formatDate(selectedDate)}</div>
                     <div className='kcal'>소모 칼로리 : {dateKcal} Kcal</div>
-                   
+                    */}
+                    <table className="kcal-table">
+  <tbody>
+    <tr>
+      <td>TODAY KCAL</td>
+      <td>    {todayKcal} Kcal</td>
+    </tr>
+    <tr>
+      <td>TOTAL KCAL</td>
+      <td>    {totalKcal} Kcal</td>
+    </tr>
+    <tr>
+      <td>SELECTED DATE</td>
+      <td>    {formatDate(selectedDate)}</td>
+    </tr>
+    <tr>
+      <td>SELECTED KCAL</td>
+      <td>    {dateKcal} Kcal</td>
+    </tr>
+  </tbody>
+</table>
+
                     {/* <div>
                         <div className="text-gray-500 mt-4">
                           선택 날짜:
